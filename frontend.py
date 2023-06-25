4eefa9dd1157e3398d796727bb526cabf7e41315
Baabaika <129168115+Baabaika@users.noreply.github.com>
2023-06-25 19:26:48 +0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -35,7 +35,7 @@ class Botinterface:
                 if event.text.lower() == "привет":
                     """БЕРЁМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ"""
                     self.params = self.vk_tools.get_profile_info(event.user_id)
-                    self.write_msg(event.user_id, f'Хай, {self.params["name"]}')
+                    self.write_msg(event.user_id, f'Хай, {self.params["name"]}, {self.params["year"]}')
                     """ПРОВЕРКА НАЛИЧИЯ ГОРОДА"""
                     if self.params["city"] == None:
                         self.write_msg(event.user_id, f'введите ваш город для поиска')
@@ -44,15 +44,15 @@ class Botinterface:
                                 self.params["city"] = event.text.replace('-', ' ').title()
                                 self.write_msg(event.user_id, f'город добавлен, {self.params["name"]}, {self.params["city"]}')
                                 break
-                    # """ПРОВЕРКА НАЛИЧИЯ ВОЗРАСТА"""
-                    # if self.params["bdate"] == None:
-                    #     self.write_msg(event.user_id, f'введите ваш возраст для поиска')
-                    #     for event in longpoll.listen():
-                    #         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                    #             if event.text.isdigit():
-                    #                 self.params["bdate"] = int(event.text.title())
-                    #                 self.write_msg(event.user_id, f'возраст добавлен, {self.params["name"]}, {self.params["city"]}, {self.params["bdate"]}')
-                    #                 break
+                    """ПРОВЕРКА НАЛИЧИЯ ВОЗРАСТА"""
+                    if self.params["year"] == None:
+                        self.write_msg(event.user_id, f'введите ваш год рождения для поиска')
+                        for event in longpoll.listen():
+                            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                if event.text.isdigit():
+                                    self.params["year"] = int(event.text.title())
+                                    self.write_msg(event.user_id, f'возраст добавлен, {self.params["name"]}, {self.params["city"]}, {self.params["year"]}')
+                                    break
                 elif event.text.lower() == "поиск":
                     self.write_msg(event.user_id, "начнем поиск")
                     """БОТ ИЩЕТ АНКЕТЫ ПО ДАННЫМ ПОЛЬЗОВАТЕЛЯ"""
@@ -69,21 +69,21 @@ class Botinterface:
                         self.worksheets = self.vk_tools.user_serch(self.params, self.offset)
                         worksheet = self.worksheets.pop()
                         while base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == True:
-                            self.worksheets = self.vk_tools.user_serch(self.params, self.offset)
-                            worksheet = self.worksheets.pop()
-                        photos = self.vk_tools.photos_get(worksheet["id"])
-                        photo_string = ''
-                        for photo in photos:
-                            photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                            if len(self.worksheets):
+                                worksheet = self.worksheets.pop()
+                            photos = self.vk_tools.photos_get(worksheet["id"])
+                            photo_string = ''
+                            for photo in photos:
+                                photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
                             self.offset += 30
-                    if base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == False:
-                        self.write_msg(event.user_id, f'имя: {worksheet["name"]} ссылка: vk.com/id{worksheet["id"]}', attachment=photo_string)
-                        self.write_msg(event.user_id, base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]))
                         """ЗАПИСЬ АНКЕТЫ В БАЗУ ДАННЫХ"""
+                    self.write_msg(event.user_id, f'имя: {worksheet["name"]} ссылка: vk.com/id{worksheet["id"]}', attachment=photo_string)
+                    self.write_msg(event.user_id, base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]))
+                    if base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == False:
                         base.add_user(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
-                    # if event.text.lower() == "в избранное":
+                    # elif event.text.lower() == "в избранное":
                     #     base.add_favorites(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
-                    # if event.text.lower() == "в черный список":
+                    # elif event.text.lower() == "в черный список":
                     #     base.add_black_list(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
                 elif event.text.lower() == "пока":
                     self.write_msg(event.user_id, "Пока((")
@@ -96,8 +96,3 @@ if __name__ == '__main__':
     bot = Botinterface(comunity_token, access_token)
     bot.handler()
 
-
-
-if __name__ == '__main__':
-    bot = Botinterface(comunity_token, access_token)
-    bot.handler()
