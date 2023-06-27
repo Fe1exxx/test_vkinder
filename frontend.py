1ed9429cea16158ebfba92a97ff170b471f0a371
Baabaika <129168115+Baabaika@users.noreply.github.com>
2023-06-27 21:49:49 +0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -46,45 +46,34 @@ class Botinterface:
                                 break
                     """ПРОВЕРКА НАЛИЧИЯ ВОЗРАСТА"""
                     if self.params["year"] == None:
-                        self.write_msg(event.user_id, f'введите ваш год рождения для поиска')
+                        self.write_msg(event.user_id, f'введите ваш возраст для поиска')
                         for event in longpoll.listen():
                             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                                 if event.text.isdigit():
                                     self.params["year"] = int(event.text.title())
                                     self.write_msg(event.user_id, f'возраст добавлен, {self.params["name"]}, {self.params["city"]}, {self.params["year"]}')
                                     break
-                elif event.text.lower() == "поиск":
+                elif event.text.lower() == 'поиск':
                     self.write_msg(event.user_id, "начнем поиск")
                     """БОТ ИЩЕТ АНКЕТЫ ПО ДАННЫМ ПОЛЬЗОВАТЕЛЯ"""
-                    if self.worksheets:
-                        worksheet = self.worksheets.pop()
-                        while base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == True:
-                            worksheet = self.worksheets.pop()
-                        """БЕРЁМ 3 ФОТО ИЗ ВЫБРАННОЙ АНКЕТЫ"""
-                        photos = self.vk_tools.photos_get(worksheet["id"])
-                        photo_string = ''
-                        for photo in photos:
-                            photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
-                    else:
-                        self.worksheets = self.vk_tools.user_serch(self.params, self.offset)
-                        worksheet = self.worksheets.pop()
-                        while base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == True:
-                            if len(self.worksheets):
-                                worksheet = self.worksheets.pop()
-                            photos = self.vk_tools.photos_get(worksheet["id"])
-                            photo_string = ''
-                            for photo in photos:
-                                photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                    marker = True
+                    while marker:
+                        if len(self.worksheets) == 0:
+                            self.worksheets = self.vk_tools.user_serch(self.params, self.offset)
                             self.offset += 30
-                        """ЗАПИСЬ АНКЕТЫ В БАЗУ ДАННЫХ"""
-                    self.write_msg(event.user_id, f'имя: {worksheet["name"]} ссылка: vk.com/id{worksheet["id"]}', attachment=photo_string)
-                    self.write_msg(event.user_id, base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]))
-                    if base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == False:
-                        base.add_user(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
-                    # elif event.text.lower() == "в избранное":
-                    #     base.add_favorites(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
-                    # elif event.text.lower() == "в черный список":
-                    #     base.add_black_list(engine=engine, profile_id=event.user_id, worksheet_id=worksheet["id"])
+                        worksheet = self.worksheets.pop()
+                        if base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]) == False:
+                            photos = self.vk_tools.photos_get(worksheet['id'])
+                            photo_string = ''
+                            for num, photo in enumerate(photos):
+                                photo_string += f'photo{photo["owner_id"]}_{photo["id"]},'
+                                if num == 2:
+                                    break
+                            self.write_msg(event.user_id, f'имя: {worksheet["name"]} ссылка: vk.com/id{worksheet["id"]}', attachment=photo_string)
+                            self.write_msg(event.user_id, base.check_user(engine, profile_id=event.user_id, worksheet_id=worksheet["id"]))
+                            """ДОБАВЛЯЕМ В БАЗУ ДАННЫХ"""
+                            base.add_user(engine, event.user_id, worksheet["id"])
+                            marker = False
                 elif event.text.lower() == "пока":
                     self.write_msg(event.user_id, "Пока((")
                 else:
